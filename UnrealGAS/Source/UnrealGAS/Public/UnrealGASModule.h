7f6b1695365dde7565bfdfe4cf6d052b93bd0d95
Eu4ng <dnjsvltm911@gmail.com>
2023-02-28 20:05:09 +0900

    Gameplay Ability Test with Meteor

--- UnrealGAS/Source/UnrealGAS/Public/UnrealGASModule.h
+++ UnrealGAS/Source/UnrealGAS/Public/UnrealGASModule.h
@@ -9,5 +9,10 @@ enum class EGASAbilityID : uint8
 {
 	None UMETA(DisplayName = "None"),
 	Confirm UMETA(DisplayName = "Confirm"),
-	Cancel UMETA(DisplayName = "Cancel")
+	Cancel UMETA(DisplayName = "Cancel"),
+	Ability1 UMETA(DisplayName = "Ability1"),
+	Ability2 UMETA(DisplayName = "Ability2"),
+	Ability3 UMETA(DisplayName = "Ability3"),
+	Ability4 UMETA(DisplayName = "Ability4"),
+	Ability5 UMETA(DisplayName = "Ability5")
 };
