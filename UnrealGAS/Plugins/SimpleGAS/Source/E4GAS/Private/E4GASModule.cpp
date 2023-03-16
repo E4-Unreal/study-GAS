1135b9a4b93195a63b2da2af89d71b93abac7612
Eu4ng <dnjsvltm911@gmail.com>
2023-03-16 13:38:50 +0900

    Modular Gameplay Features (Abilities) Init Add Plugin 'SimpleMGF

--- UnrealGAS/Plugins/SimpleGAS/Source/E4GAS/Private/E4GASModule.cpp
+++ UnrealGAS/Plugins/SimpleGAS/Source/E4GAS/Private/E4GASModule.cpp
@@ -1,14 +1,17 @@
 #include "E4GASModule.h"
 
+#include "AbilitySystemGlobals.h"
 #include "GASGameplayTags.h"
 #include "Modules/ModuleManager.h"
 
 #define LOCTEXT_NAMESPACE "FTemplateModule"
 
+// Instead of UGASAssetManager::StartInitialLoading()
 void FE4GASModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
 	FGASGameplayTags::InitializeNativeTags();
+	//UAbilitySystemGlobals::Get().InitGlobalData();
 }
 
 void FE4GASModule::ShutdownModule()
